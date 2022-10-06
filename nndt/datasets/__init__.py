0c95d06b14a6d6c37d3bba5e1af4ad141265d0e3
Dopervigest <vladislav0860@gmail.com>
2022-10-06 12:23:09 +0500

    dataset source folder

---
+++ nndt/datasets/__init__.py
@@ -0,0 +1,5 @@
+from amos import AMOS_5
+
+list_of_datasets = [
+    'amos_5',
+]
---
+++ nndt/datasets/amos.py
@@ -0,0 +1,10 @@
+class AMOS_5():
+    url = 'https://drive.google.com/file/d/1Bbeo6tWetHrEsDlMw3G7mBJU4P8RZhdb/view?usp=sharing'
+
+
+class AMOS_10():
+    url = ''
+
+
+class AMOS_100():
+    url = ''
