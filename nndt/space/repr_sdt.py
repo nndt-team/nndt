Fix issues with precision in test. PEP8

nndt/space/repr_sdt.py
@@ -4,6 +4,7 @@ import warnings
 import jax
 import jax.numpy as jnp
 import optax
+from packaging import version
 from tqdm import tqdm
 
 import nndt
@@ -13,8 +14,6 @@ from nndt.space.sources import SDTSource, SDFPKLSource
 from nndt.space.utils import downup_update_bbox
 from nndt.space.vtk_wrappers import *
 from nndt.trainable_task import SimpleSDF
-
-from packaging import version
 
 
 class AbstractSDXRepr(AbstractRegion, ExtendedNodeMixin, UnloadMixin):
@@ -220,7 +219,6 @@ class TrainSDT2SDF(AbstractMethod, ExtendedNodeMixin):
 
 
 class SDFRepr(AbstractSDXRepr):
-
     MAGIC_CORRECTION = 0.503  # This is absolutely magic coefficient that reduce error between bboxes to 0.49075 mm
 
     def __init__(self, parent: SDFPKLSource,
@@ -263,14 +261,15 @@ class SDFRepr(AbstractSDXRepr):
 
     def ns_xyz2sdt(self, ns_xyz: onp.ndarray) -> onp.ndarray:
         ret_shape = list(ns_xyz.shape)
-        ret_shape[-1] = 1; ret_shape = tuple(ret_shape)
+        ret_shape[-1] = 1;
+        ret_shape = tuple(ret_shape)
 
-        ns_xyz_flat = ns_xyz.reshape((-1,3))
+        ns_xyz_flat = ns_xyz.reshape((-1, 3))
         rng = jax.random.PRNGKey(42)
         ns_sdf = self.F.vec_sdf(self.trainable_params, rng,
-                              ns_xyz_flat[:, 0],
-                              ns_xyz_flat[:, 1],
-                              ns_xyz_flat[:, 2])
+                                ns_xyz_flat[:, 0],
+                                ns_xyz_flat[:, 1],
+                                ns_xyz_flat[:, 2])
 
         ns_sdf = ns_sdf.reshape(ret_shape)
         return ns_sdf
@@ -299,13 +298,13 @@ class SDFRepr(AbstractSDXRepr):
         task = SimpleSDF(**trainable_task_)
 
         sdf_repr = SDFRepr(parent=source,
-                           trainable_task = task,
-                           trainable_params = params_,
-                           physical_center = repr_["physical_center"],
-                           physical_bbox = repr_["physical_bbox"],
-                           normed_center = repr_["normed_center"],
-                           normed_bbox = repr_["normed_bbox"],
-                           scale_physical2normed = repr_["scale_physical2normed"])
+                           trainable_task=task,
+                           trainable_params=params_,
+                           physical_center=repr_["physical_center"],
+                           physical_bbox=repr_["physical_bbox"],
+                           normed_center=repr_["normed_center"],
+                           normed_bbox=repr_["normed_bbox"],
+                           scale_physical2normed=repr_["scale_physical2normed"])
 
         downup_update_bbox(sdf_repr)
 

tests/test_space2_primitive_sdf.py
@@ -2,9 +2,9 @@ import unittest
 
 import nndt.space2 as spc
 from nndt.primitive_sdf import *
-import jax.numpy as jnp
 
 PATH_TEST_ACDC = './acdc_for_test'
+
 
 class PrimitiveSDFTestCase(unittest.TestCase):
 
@@ -58,6 +58,7 @@ class PrimitiveSDFTestCase(unittest.TestCase):
                                           space.for_test.surface_xyz2sdt((jnp.array([1., 1., 2.]))))))
         self.assertTrue(((0., 0., 0.), (2., 2., 2.)), space.for_test.bbox)
         print(space.print('full'))
+
 
 if __name__ == '__main__':
     unittest.main()

tests/test_space2_transformation.py
@@ -74,10 +74,14 @@ class MethodSetTestCase(unittest.TestCase):
         ps_grid = grid_in_cube2(spacing=(2, 2, 2), lower=(59.0, 112.0, 7.0), upper=(134.0, 183.0, 84.0))
         ns_grid = grid_in_cube2(spacing=(2, 2, 2), lower=(-0.75, -0.71, -0.77), upper=(0.75, 0.71, 0.77))
 
-        self.assertAlmostEqual(-0.7699, float(jnp.array(space.patient009.transform.transform_xyz_ps2ns(ps_grid)).min()), places=2)
-        self.assertAlmostEqual(0.7699, float(jnp.array(space.patient009.transform.transform_xyz_ps2ns(ps_grid)).max()), places=2)
-        self.assertAlmostEqual(7.0, float(jnp.array(space.patient009.transform.transform_xyz_ns2ps(ns_grid)).min()), places=2)
-        self.assertAlmostEqual(183.0, float(jnp.array(space.patient009.transform.transform_xyz_ns2ps(ns_grid)).max()), places=2)
+        self.assertAlmostEqual(-0.7699, float(jnp.array(space.patient009.transform.transform_xyz_ps2ns(ps_grid)).min()),
+                               places=2)
+        self.assertAlmostEqual(0.7699, float(jnp.array(space.patient009.transform.transform_xyz_ps2ns(ps_grid)).max()),
+                               places=2)
+        self.assertAlmostEqual(7.0, float(jnp.array(space.patient009.transform.transform_xyz_ns2ps(ns_grid)).min()),
+                               places=2)
+        self.assertAlmostEqual(183.0, float(jnp.array(space.patient009.transform.transform_xyz_ns2ps(ns_grid)).max()),
+                               places=2)
 
     def test_transform_inversions(self):
         space = self.helper_transform_load(mode="identity")
@@ -85,24 +89,32 @@ class MethodSetTestCase(unittest.TestCase):
         inv_a = space.patient009.transform.transform_xyz_ps2ns
         b = space.patient009.transform.transform_xyz_ns2ps
         inv_b = space.patient009.transform.transform_xyz_ps2ns
-        self.assertEqual((3., 4., 5.), tuple(inv_a(a(jnp.array((3., 4., 5.))))))
-        self.assertEqual((1., 2., 3.), tuple(inv_b(b(jnp.array((1., 2., 3.))))))
+        self.assertTrue(bool(jnp.allclose(jnp.array((3., 4., 5.)),
+                                          inv_a(a(jnp.array((3., 4., 5.)))))))
+        self.assertTrue(bool(jnp.allclose(jnp.array((1., 2., 3.)),
+                                          inv_b(b(jnp.array((1., 2., 3.)))))))
 
         space = self.helper_transform_load(mode="shift_and_scale")
         a = space.patient009.transform.transform_xyz_ns2ps
         inv_a = space.patient009.transform.transform_xyz_ps2ns
         b = space.patient009.transform.transform_xyz_ns2ps
         inv_b = space.patient009.transform.transform_xyz_ps2ns
-        self.assertEqual((3., 4., 5.), tuple(inv_a(a(jnp.array((3., 4., 5.))))))
-        self.assertEqual((1., 2., 3.), tuple(inv_b(b(jnp.array((1., 2., 3.))))))
+        self.assertTrue(bool(jnp.allclose(jnp.array((3., 4., 5.)),
+                                          inv_a(a(jnp.array((3., 4., 5.)))))))
+        self.assertTrue(bool(jnp.allclose(jnp.array((1., 2., 3.)),
+                                          inv_b(b(jnp.array((1., 2., 3.)))))))
 
         space = self.helper_transform_load(mode="to_cube")
         a = space.patient009.transform.transform_xyz_ns2ps
         inv_a = space.patient009.transform.transform_xyz_ps2ns
         b = space.patient009.transform.transform_xyz_ns2ps
         inv_b = space.patient009.transform.transform_xyz_ps2ns
-        self.assertEqual((3., 4., 5.), tuple(inv_a(a(jnp.array((3., 4., 5.))))))
-        self.assertEqual((1., 2., 3.), tuple(inv_b(b(jnp.array((1., 2., 3.))))))
+
+        self.assertTrue(bool(jnp.allclose(jnp.array((3., 4., 5.)),
+                                          inv_a(a(jnp.array((3., 4., 5.)))))))
+        self.assertTrue(bool(jnp.allclose(jnp.array((1., 2., 3.)),
+                                          inv_b(b(jnp.array((1., 2., 3.)))))))
+
 
 if __name__ == '__main__':
     unittest.main()