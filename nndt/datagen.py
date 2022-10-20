81043ca57fb38a26dde60a384d844b71b1439c65
konstantin.ushenin <kostaNew@gmail.com>
2022-10-21 00:22:56 +0500

    Datagen, NO TESTS

--- nndt/datagen.py
+++ nndt/datagen.py
@@ -1,10 +1,12 @@
+import time
+
 import jax
 import jax.numpy as jnp
 
 import nndt
 
 
-def rotation_matrix(yaw, pitch, roll):
+def _rotation_matrix(yaw, pitch, roll):
     Rz = jnp.array([[jnp.cos(yaw), -jnp.sin(yaw), 0.],
                     [jnp.sin(yaw), jnp.cos(yaw), 0.],
                     [0., 0., 1.]])
@@ -18,20 +20,30 @@ def rotation_matrix(yaw, pitch, roll):
     return Rz @ Ry @ Rx
 
 
-def scale_and_rotate(xyz, scale=(1., 1., 1.), rotate=(0., 0., 0.)):
-    assert (xyz.shape[-1] == 3)
-    M = rotation_matrix(*rotate)
-    scale = jnp.array(scale)
-
-    ret_shape = xyz.shape
-    xyz = xyz.reshape((-1, 3))
-    xyz = (M @ (xyz * scale).T).T
-
-    xyz = xyz.reshape(ret_shape)
-    return xyz
+def _scale_xyz(cube, scale):
+    return scale * cube
 
 
-class DataGenerator:
+def _rotate_xyz(cube, M):
+    M = _rotation_matrix(M[0], M[1], M[2]).T
+    return cube @ M
+
+
+def _shift_xyz(cube, shift):
+    return cube + shift
+
+
+def _scale_rotate_shift(cube, scale=(1., 1., 1.), rotation=(0., 0., 0.), shift=(0., 0., 0.)):
+    cube = _scale_xyz(cube, scale)
+    cube = _rotate_xyz(cube, rotation)
+    cube = _shift_xyz(cube, shift)
+    return cube
+
+
+_vec_scale_rotate_shift = jax.jit(jax.vmap(_scale_rotate_shift, (0, 0, 0, 0)))
+
+
+class DataGenerator2:
 
     def __init__(self,
                  node,
@@ -39,76 +51,103 @@ class DataGenerator:
                  cube_scale=1.,
                  count=33,
                  step=77,
-                 sigma=0.09,
+                 shift_sigma=0.09,
+                 scale_range=0.03,
+                 rotate_angle=0.03,
                  shift_mul=4,
-                 rand_scale_sub=0.1, augment=True,
-                 rotation=1):
+                 augment=True):
         self.node = node
         self.count = count
         self.step = step
-        self.sigma = sigma
-        self.shift_mul = shift_mul
-        self.ns_cube = nndt.math_core.grid_in_cube(spacing=cube_spacing,
-                                                   scale=cube_scale,
-                                                   center_shift=(0., 0., 0.))
-        self.rotation = rotation
-
-        self.rand_scale_sub = rand_scale_sub
         self.augment = augment
+        self.shift_mul = shift_mul
 
-    def _process_one_model(self, key, obj, epoch, batch_color_class: list, batch_sdt: list):
+        if self.augment:
+            self.shift_sigma = shift_sigma
+            self.scale_range = scale_range
+            self.rotate_angle = rotate_angle
+        else:
+            self.shift_sigma = 0.
+            self.scale_range = 0.
+            self.rotate_angle = 0.
 
+        one_cube = nndt.math_core.grid_in_cube(spacing=cube_spacing,
+                                               scale=cube_scale,
+                                               center_shift=(0., 0., 0.))
+        self.basic_cube = jnp.repeat(one_cube[jnp.newaxis, ...], self.count, axis=0)
+
+    def _process_one_model(self, key, obj, epoch):
+        key, subkey = jax.random.split(key)
         ind, xyz = obj.sampling_eachN_from_mesh(count=self.count,
                                                 step=self.step,
                                                 shift=self.shift_mul * epoch)
         key, subkey = jax.random.split(key)
-        if self.augment:
-            xyz = xyz + self.sigma * jax.random.normal(subkey, shape=xyz.shape)
-        else:
-            pass
-        rgba = obj.surface_xyz2rgba(xyz)
+        scale = jax.random.uniform(subkey, shape=(self.count, 3),
+                                   minval=1. - self.scale_range, maxval=1. + self.scale_range)
+        key, subkey = jax.random.split(key)
+        rotate = jax.random.uniform(subkey, shape=(self.count, 3),
+                                    minval=0. - self.rotate_angle, maxval=0. + self.rotate_angle)
+        key, subkey = jax.random.split(key)
+        shift = jax.random.normal(subkey, shape=(self.count, 3)) * self.shift_sigma
+
+        new_pos_xyz = xyz + shift
+        _xyz_cube = _vec_scale_rotate_shift(self.basic_cube, scale, rotate, new_pos_xyz)
+
+        rgba = obj.surface_xyz2rgba(new_pos_xyz)
         color_class = jnp.argmax(rgba[:, 0:3], axis=1)
-        batch_color_class.append(color_class)
+        sdt = obj.surface_xyz2sdt(_xyz_cube)
 
-        if self.augment:
-            scale_list = jax.random.uniform(subkey, shape=(len(xyz), 3,),
-                                            minval=1. - self.rand_scale_sub,
-                                            maxval=1. + self.rand_scale_sub)
-            rotate_list = jax.random.uniform(subkey, shape=(len(xyz), 3,),
-                                             minval=-self.rotation,
-                                             maxval=self.rotation)
-
-        else:
-            scale_list = [(1., 1., 1.)] * len(xyz)
-            rotate_list = [(0., 0., 0.)] * len(xyz)
-
-        batch_sdtlocal = []
-        for idx, point in enumerate(xyz):
-            scale = scale_list[idx]
-            rotate = rotate_list[idx]
-            ns_cube = scale_and_rotate(self.ns_cube, scale=scale, rotate=rotate)
-            ns_cube = ns_cube + point
-            sdtlocal = obj.surface_xyz2sdt(ns_cube)
-
-            batch_sdtlocal.append(sdtlocal)
-        batch_sdt.append(jnp.stack(batch_sdtlocal, axis=0))
-
-        return batch_color_class, batch_sdt, subkey
+        return subkey, color_class, sdt
 
     def get(self, key, epoch, index=None):
         batch_color_class = []
         batch_sdt = []
+        lst = [index] if index is not None else range(len(self.node))
 
-        if index is None:
-            for obj in self.node:
-                batch_color_class, batch_sdt, subkey = self._process_one_model(key, obj, epoch, batch_color_class,
-                                                                               batch_sdt)
-        else:
-            obj = self.node[index]
-            batch_color_class, batch_sdt, subkey = self._process_one_model(key, obj, epoch, batch_color_class,
-                                                                           batch_sdt)
+        key, subkey = jax.random.split(key)
+
+        for i in lst:
+            obj = self.node[i]
+            subkey, color_class, sdt = self._process_one_model(key, obj, epoch)
+            batch_color_class.append(color_class)
+            batch_sdt.append(sdt)
 
         batch_color_class = jnp.concatenate(batch_color_class, axis=0)
         batch_sdt = jnp.concatenate(batch_sdt, axis=0)
 
-        return batch_sdt, batch_color_class, subkey
+        return batch_sdt, batch_color_class
+
+
+if __name__ == '__main__':
+    from nndt.space2 import load_from_path
+
+    space = load_from_path('./../tests/acdc_for_test')
+    space.preload("shift_and_scale")
+    print(space.print())
+
+    key = jax.random.PRNGKey(42)
+    dg = DataGenerator2(space, augment=False)
+    dg.get(key, 0)
+
+    start_time = time.time()
+    batch_sdt, batch_color_class = dg.get(key, 10)
+    stop_time = time.time()
+    print((stop_time - start_time) / 5)
+
+    dg = DataGenerator2(space, augment=True)
+    dg.get(key, 0)
+
+    start_time = time.time()
+    key = jax.random.PRNGKey(42)
+    batch_sdt, batch_color_class = dg.get(key, 10)
+    stop_time = time.time()
+    print((stop_time - start_time) / 5)
+
+    # plt.imshow(batch_sdt[20, :, :, 8, 0])
+    # plt.show()
+    # plt.imshow(batch_sdt[20, :, 8, :, 0])
+    # plt.show()
+    # plt.imshow(batch_sdt[20, 8, :, :, 0])
+    # plt.show()
+    # print(batch_sdt[20, :, :, 8, 0])
+    # print(batch_sdt.shape, batch_color_class.shape)
