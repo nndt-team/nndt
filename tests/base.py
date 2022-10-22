70b6f114902c1fff49e43e2a6d90892d3007606a
konstantin.ushenin <kostaNew@gmail.com>
2022-10-22 18:53:06 +0500

    SHIT!

--- tests/base.py
+++ tests/base.py
@@ -2,21 +2,23 @@ import os
 import unittest
 
 from py7zr import py7zr
+from typing import Final
 
-PATH_TEST_STRUCTURE = './tree_for_test'
-PATH_TEST_ACDC = './acdc_for_test'
+PATH_TEST_STRUCTURE: Final[str] = './tree_for_test'
+PATH_TEST_ACDC: Final[str] = './acdc_for_test'
 
 
 class BaseTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print("WORKING DIR: " + os.getcwd())
+        print("SHIT: " + os.listdir('../tests/'))
 
         src_target = [PATH_TEST_STRUCTURE, PATH_TEST_ACDC]
         if os.path.basename(os.getcwd()) == 'tests':
             src_file = [PATH_TEST_STRUCTURE + '.7z', PATH_TEST_ACDC + '.7z']
         elif os.getcwd() == '/home/runner/work/nndt/nndt':  # TODO this is shit!
-            src_file = ['../tests/' + PATH_TEST_STRUCTURE + '.7z', '../tests/' + PATH_TEST_ACDC + '.7z']
+            src_file = ['tests/' + PATH_TEST_STRUCTURE + '.7z', 'tests/' + PATH_TEST_ACDC + '.7z']
         elif os.path.basename(os.getcwd()) == 'nndt':
             src_file = ['./tests/' + PATH_TEST_STRUCTURE + '.7z', './tests/' + PATH_TEST_ACDC + '.7z']
         else:
