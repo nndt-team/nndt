3171cba4a2cf0916c8842544b17e5376f4b751e1
Mihail <321mig123@gmail.com>
2022-10-22 22:16:40 +0500

    3_utils documentation

--- nndt/space2/tree_utils.py
+++ nndt/space2/tree_utils.py
@@ -5,12 +5,24 @@ from nndt.space2.utils import update_bbox
 
 
 def update_bbox_from_children(node):
+    """
+    Update box size for node. Count childrens size and update current node size.
+
+    Args:
+        node : Node to update.
+    """
     for child in node.children:
         if isinstance(child, AbstractBBoxNode):
             node.bbox = update_bbox(node.bbox, child.bbox)
 
 
 def update_bbox_with_float_over_tree(node: NodeMixin):
+    """
+    Update all parents boundary box size for specified node.
+    
+    Args:
+        node (NodeMixin): start node.
+    """
     current_node = node
     while not current_node.is_root:
         bbox1 = current_node.bbox
