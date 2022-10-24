93dec05c3a71b6b5d3ff2557c528e42e50c5f3c3
KonstantinUshenin <konstantin.ushenin@urfu.ru>
2022-10-24 18:14:52 +0500

    Update filesource.py

--- nndt/space2/filesource.py
+++ nndt/space2/filesource.py
@@ -7,12 +7,12 @@ from nndt.space2.abstracts import AbstractBBoxNode, IterAccessMixin
 
 class FileSource(AbstractBBoxNode, IterAccessMixin):
     """
-    File source.
+    This class show location of file for processing.
     
     Args:
             name (_type_): file name.
             filepath (str): file path. If not exists raise FileNotFoundError.
-            loader_type (str): loader type.
+            loader_type (str): loader type, this string notes type of information for uploading 
             bbox (tuple, optional): boundary box in form ((X_min, Y_min, Z_min), (X_max, Y_max, Z_max)). 
                                     Defaults to ((0., 0., 0.), (0., 0., 0.)).
             parent (_type_, optional): parent node. Defaults to None.
