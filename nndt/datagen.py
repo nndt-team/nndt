07174de4bda69319a600932e6f16d9a30781be59
semyon-v-dot <semyon-v-dot@yandex.ru>
2022-11-01 10:09:29 +0500

    Del repeated code from math_core

--- nndt/datagen.py
+++ nndt/datagen.py
@@ -5,37 +5,9 @@ import jax.numpy as jnp
 from jax.random import KeyArray
 
 import nndt
+from nndt.math_core import rotation_matrix as _rotation_matrix
+from nndt.math_core import scale_xyz as _scale_xyz
 from nndt.trainable_task import ApproximateSDF
-
-
-def _rotation_matrix(yaw, pitch, roll):
-    Rz = jnp.array(
-        [
-            [jnp.cos(yaw), -jnp.sin(yaw), 0.0],
-            [jnp.sin(yaw), jnp.cos(yaw), 0.0],
-            [0.0, 0.0, 1.0],
-        ]
-    )
-    Ry = jnp.array(
-        [
-            [jnp.cos(pitch), 0, jnp.sin(pitch)],
-            [0, 1, 0],
-            [-jnp.sin(pitch), 0.0, jnp.cos(pitch)],
-        ]
-    )
-    Rx = jnp.array(
-        [
-            [1.0, 0.0, 0.0],
-            [0.0, jnp.cos(roll), -jnp.sin(roll)],
-            [0.0, jnp.sin(roll), jnp.cos(roll)],
-        ]
-    )
-
-    return Rz @ Ry @ Rx
-
-
-def _scale_xyz(cube, scale):
-    return scale * cube
 
 
 def _rotate_xyz(cube, M):
