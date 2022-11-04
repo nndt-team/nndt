531b47169b18a9eecad832efbc619a7ebd199b13
konstantin.ushenin <kostaNew@gmail.com>
2022-11-04 11:39:23 +0500

    Fix `.print()` and `.plot()`. print() work without print

--- nndt/__init__.py
+++ nndt/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.3b2"
+__version__ = "0.0.3rc1"
 
 from nndt.datagen import DataGenForSegmentation, DataGenForShapeRegression
 from nndt.global_config import PYVISTA_PRE_PARAMS, init_code, init_colab, init_jupyter
