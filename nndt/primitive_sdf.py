09e9af24356afcdcdf2f4c459095ad1d5600990a
semyon-v-dot <semyon-v-dot@yandex.ru>
2022-11-08 09:48:13 +0500

    Fix BoxSDF prim func

--- nndt/primitive_sdf.py
+++ nndt/primitive_sdf.py
@@ -200,8 +200,8 @@ class BoxSDF(AbstractSDF):
                 dist_to_planes_xyz += jnp.where(
                     (min_xyz[i] <= x) & (x <= max_xyz[i]),
                     jnp.min(
-                        jnp.abs(jnp.subtract(x, min_xyz[i])),
-                        jnp.abs(jnp.subtract(x, max_xyz[i])),
+                        (jnp.abs(jnp.subtract(x, min_xyz[i])),),
+                        (jnp.abs(jnp.subtract(x, max_xyz[i])),),
                     ),
                     jnp.array(()),
                 )
