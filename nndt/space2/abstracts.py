35ad1755040bb0398c4cba653c45cb751660d1f5
semyon-v-dot <semyon-v-dot@yandex.ru>
2022-11-11 10:46:10 +0500

    Revert "Fix return"
    This reverts commit 57a16038cdd40d821604c0dbe017d21ef6b8cdd7.

--- nndt/space2/abstracts.py
+++ nndt/space2/abstracts.py
@@ -221,7 +221,7 @@ class AbstractBBoxNode(AbstractTreeElement):
         """
         from nndt.space2.plot_tree import _plot
 
-        return _plot(self, mode, filepath, **kwargs)
+        _plot(self, mode, filepath, **kwargs)
 
     @node_method("unload_from_memory()")
     def unload_from_memory(self):
